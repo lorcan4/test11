47ab762f8bb7efd97ab1df1f4e57329cd51d8a80
lorcan4 <mcdochvkgmail.com>
2024-06-01 23:06:44 +0100

    Setup EJS and deploy to Vercel2

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send('Hello, Vercel!');
-});
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'api')); // Set the directory for EJS files
 
-const PORT = process.env.PORT || 3500;
+// Serve static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname,  'node.js')));
+
+
+app.get('/api1', (req, res) => {
+  res.render('index1'); // Render the index.ejs file
+});
+ 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+  
