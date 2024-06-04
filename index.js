9aea1d9d4916c41322838a3352f5d43a3dcba951
lorcan4 <mcdochvkgmail.com>
2024-06-04 22:50:05 +0100

    change 2

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
-// Set the view engine to EJS
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'api')));
+// Set EJS as templating engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname , "api")); // Set the directory for EJS files
-
-// Serve static files (CSS, JS, images) if you have any
+app.set('views', path.join(__dirname, 'api'));
 
 
-// Define a route to render the EJS page
-app.get('/api1', (req, res) => {
-  res.render('index1'); // Render the index1.ejs file
+
+ 
+const user = { 
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+};
+         
+// Routes
+app.get('/login', (req, res) => { 
+   res.render('index1');
+});     
+
+ app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === user.username && password === user.password) {
+   res.render('seccess', { username });
+  } else {
+   res.render('index1', { error: 'Invalid username or password' });
+ }
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+ 
+const PORT = process.env.PORT || 4550;
+app.listen(PORT, () => { 
   console.log(`Server is running on port ${PORT}`);
 });
+  
