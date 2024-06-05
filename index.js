01ae0c2231f085476578242f60a8f54bcaac328f
lorcan4 <mcdochvkgmail.com>
2024-06-05 16:21:37 +0100

    fi

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'api')));
-// Set EJS as templating engine
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'api'));
+app.set('views', path.join(__dirname, 'api')); // Set the directory for EJS files
+
+// Serve static files (CSS, JS, images) if you have any
 
 
-
- 
-const user = { 
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-};
-         
-// Routes
-app.get('/login', (req, res) => { 
-   res.render('index1');
-});     
-
- app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (username === user.username && password === user.password) {
-   res.render('seccess', { username });
-  } else {
-   res.render('index1', { error: 'Invalid username or password' });
- }
+// Define a route to render the EJS page
+app.get('/api1', (req, res) => {
+  res.render('index1'); // Render the index1.ejs file
 });
- 
-const PORT = process.env.PORT || 4550;
-app.listen(PORT, () => { 
-  console.log(`Server is running on port ${PORT}`);
-});
-  
+
+module.exports.handler = serverless(app);
+    
